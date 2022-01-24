242a08afb75e88842bea0e736d3e476aba14adc4
Andres Castañeda <af.castaneda223@gmail.com>
2022-01-24 15:20:34 -0500

    change viewport width

--- src/components/styles/styles.style.js
+++ src/components/styles/styles.style.js
@@ -11,7 +11,7 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    @media screen and (max-width: 800px) {
+    @media screen and (max-width: 1200px) {
         width: 60vw;
       }
       @media screen and (max-width: 800px) {
